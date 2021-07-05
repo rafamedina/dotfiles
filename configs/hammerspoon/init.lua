7cc3ef8865e40ed02dfe9f30d711840bd6298d90
Rafael Medina <rafaelmedina789@gmail.com>
2021-07-05 12:46:02 -0600

    Polishing hammerspoon

--- configs/hammerspoon/init.lua
+++ configs/hammerspoon/init.lua
@@ -3,20 +3,7 @@ local grid = require "grid"
 hs.window.animationDuration = 0 -- disable animations
 
 -- Forward function declarations.
-local activate = nil
-local activateLayout = nil
-local canManageWindow = nil
 local chain = nil
-local handleScreenEvent = nil
-local handleWindowEvent = nil
-local hide = nil
-local initEventHandling = nil
-local internalDisplay = nil
-local tearDownEventHandling = nil
-local windowCount = nil
-
-local screenCount = #hs.screen.allScreens()
-
 local lastSeenChain = nil
 local lastSeenWindow = nil
 local lastSeenAt = nil
@@ -142,8 +129,48 @@ hs.hotkey.bind({'ctrl', 'alt'}, '6', (function()
   grid.app(grid.Layout.bottomLastThird)
 end))
 
+hs.hotkey.bind({'ctrl', 'alt'}, '7', (function()
+  grid.app(grid.Layout.topTwoThirds)
+end))
+
+hs.hotkey.bind({'ctrl', 'alt'}, '8', (function()
+  grid.app(grid.Layout.topLastTwoThirds)
+end))
+
+hs.hotkey.bind({'ctrl', 'alt'}, '9', (function()
+  grid.app(grid.Layout.bottomTwoThirds)
+end))
+
+hs.hotkey.bind({'ctrl', 'alt'}, '0', (function()
+  grid.app(grid.Layout.bottomLastTwoThirds)
+end))
+
 hs.hotkey.bind({'cmd'}, 'escape', (function()
   hs.hints.windowHints()
 end))
+
+function applicationWatcher(appName, eventType, appObject)
+  if (eventType == hs.application.watcher.activated) then
+      if (appName == "Finder") then
+          -- Bring all Finder windows forward when one gets activated
+          appObject:selectMenuItem({"Window", "Bring All to Front"})
+      end
+  end
+end
+local appWatcher = hs.application.watcher.new(applicationWatcher)
+appWatcher:start()
+
+function reloadConfig(files)
+  doReload = false
+  for _,file in pairs(files) do
+      if file:sub(-4) == ".lua" then
+          doReload = true
+      end
+  end
+  if doReload then
+      hs.reload()
+  end
+end
+local myWatcher = hs.pathwatcher.new(os.getenv("HOME") .. "/.hammerspoon/", reloadConfig):start()
 
 hs.alert.show("Config loaded")
